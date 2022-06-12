11a20b6063fd4bf0086c0aa0308efc089ab5ca7f
ashkan <ashkantavangar7@gmail.com>
2022-06-12 16:27:03 +0430

    clean up code and debugging

--- script/script.js
+++ script/script.js
@@ -3,14 +3,14 @@ const formInput = document.getElementById('form-input');
 const notesInput = document.getElementById('notes-input');
 const notesBox = document.getElementById('notes-box');
 //////local storage datas
-let id = 0;
+let lastId = 0;
 let description = '';
 const notes = [];
 const localStorageItems = JSON.parse(localStorage.getItem('notes-data'));
 ////////
 if (localStorageItems) {
     localStorageItems.map((localStorageItem) => {
-        id = localStorageItem.id;
+        lastId = localStorageItem.id;
         notes.push(localStorageItem);
         notesBox.innerHTML = '';
         showDOM();
@@ -23,12 +23,12 @@ function showDOM() {
         const savedNoteBox = document.createElement('div');
         savedNoteBox.textContent = item.description;
         savedNoteBox.classList.add('saved-notes');
-        delButton.classList.add('button-del');
         notesBox.appendChild(savedNoteBox);
         /////show  delete button
         const delButton = document.createElement('button');
+        delButton.classList.add('button-del');
         delButton.classList.add('button');
-        delButton.textContent = 'delete'
+        delButton.textContent = 'delete';
         savedNoteBox.appendChild(delButton);
         ////////delete from dom and local storage  
         delButton.onclick = () => {
@@ -36,21 +36,21 @@ function showDOM() {
             notes.splice(item.id - 1, 1);
             for (let i = 0; i < notes.length; i++) {
                 if (i >= item.id - 1) {
-                    localStorage.setItem('notes-data', JSON.stringify(notes));
-                    console.log(notes);
+                    notes[i].id-=1;
+                    lastId=notes[i].id;
                 }
             }
-
+            localStorage.setItem('notes-data', JSON.stringify(notes));
         };
     })
 }
 ///////pushing
 const pushNotes = () => {
-    id += 1
-    if (id > 0 && description) {
-        notes.push({ id, description });
+    lastId += 1
+    if (lastId > 0 && description) {
+        notes.push({ id:lastId, description });
         localStorage.setItem('notes-data', JSON.stringify(notes));
-        console.log(notes);
+        alert('data saved');
     }
 }
 /////submit handle
